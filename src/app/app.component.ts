7f9fb78dfc06f987709185b0c9e4041861d81d63
Nesreen <nesreenmhd@gmail.com>
2020-05-23 18:12:23 +0200

    Template Driven Example in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tempalate-form';
+  @ViewChild('f') submittedForm: NgForm;
+  default_level: string = 'Advanced';
+  userData = {
+    userEmail: '',
+    userPassword: '',
+    userLevel: '',
+  };
+  validForm: boolean = false;
+
+  levels: string[] = ['Basic', 'Advanced', 'Professional'];
+
+  onSubmit() {
+    this.validForm = true;
+    this.userData.userEmail = this.submittedForm.value.email;
+    this.userData.userPassword = this.submittedForm.value.password;
+    this.userData.userLevel = this.submittedForm.value.level;
+  }
 }
